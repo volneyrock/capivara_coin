23ae45f883d082e3f48767b028b54ee5bf8e013d
Volney Casas <volneyrock@gmail.com>
2023-05-21 10:24:48 -0300

    New token created

--- CapivaraCoin.sol
+++ CapivaraCoin.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "./ERC20.sol";
 
 contract CapivaraCoin is ERC20 {
-    constructor() ERC20("Capivara Coin", "CAP") {
+    constructor() ERC20("Capivara Coin", "CAPC") {
         _mint(msg.sender, 10000000000 * 10 ** decimals());
     }
 }
